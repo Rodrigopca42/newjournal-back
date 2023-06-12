e0e6b4b936e127a7d0d791573adc48f357d1ef5b
Rodrigopca42 <rodrigopca426281@gmail.com>
2023-06-12 10:39:49 -0300

    projeto criado

---
+++ src/test/java/com/rodrigo/newJournal2/NewJournal2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rodrigo.newJournal2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewJournal2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
